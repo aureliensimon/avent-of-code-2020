66473869c101207a877eb1f2de1428bf4aba62e4
aureliensimon <aureliensimon29260@gmail.com>
2020-12-04 12:10:57 +0100

    fix day4 last passport

--- challenges/04/4.py
+++ challenges/04/4.py
@@ -6,11 +6,45 @@ import re
 fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
 eye_colors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
 
+def validate_infos (passport):
+    validation_infos = True
+
+    hgt = passport['hgt']
+    hcl = passport['hcl']
+    ecl = passport['ecl']
+    pid = passport['pid']
+
+    # date check
+    if (not number_is_between(1920, 2002, passport['byr'])) or (not number_is_between(2010, 2020, passport['iyr'])) or (not number_is_between(2020, 2030, passport['eyr'])):
+        validation_infos = False
+
+    # regex check
+    if not re.search('^#[0-9a-f]{6}$', hcl) or (not re.search('^[0-9]{9}$', pid)):
+        validation_infos = False
+
+    # exist in array
+    if ecl not in eye_colors:
+        validation_infos = False
+
+    # unit and height check
+    unit = hgt[-2:]
+    if (unit == 'in'):
+        if not number_is_between(59, 76, hgt[:-2]):
+            validation_infos = False
+    elif (unit == 'cm'):
+        if not number_is_between(150, 193, hgt[:-2]):
+            validation_infos = False
+    else:
+        validation_infos = False
+
+
+    return validation_infos
+
 def number_is_between(lower, upper, number):
     return lower <= int(number) <= upper
 
 def part1 (fa):
-    t = 1
+    t = 0
     passport = {}
 
     for l in fa:
@@ -25,10 +59,12 @@ def part1 (fa):
             t += all([fd in passport for fd in fields])
             passport = {}
 
+    t += all([fd in passport for fd in fields])
+
     return t
 
 def part2 (fb):
-    t = 1
+    t = 0
     passport = {}
 
     for l in fb:
@@ -43,41 +79,12 @@ def part2 (fb):
             all_infos = all([fd in passport for fd in fields])
 
             if all_infos:
-                validation_infos = True
-
-                hgt = passport['hgt']
-                hcl = passport['hcl']
-                ecl = passport['ecl']
-                pid = passport['pid']
-
-                # date check
-                if (not number_is_between(1920, 2002, passport['byr'])) or (not number_is_between(2010, 2020, passport['iyr'])) or (not number_is_between(2020, 2030, passport['eyr'])):
-                    validation_infos = False
-
-                # regex check
-                if not re.search('^#[0-9a-f]{6}$', hcl) or (not re.search('^[0-9]{9}$', pid)):
-                    validation_infos = False
-
-                # exist in array
-                if ecl not in eye_colors:
-                    validation_infos = False
-
-                # unit and height check
-                unit = hgt[-2:]
-                if (unit == 'in'):
-                    if not number_is_between(59, 76, hgt[:-2]):
-                        validation_infos = False
-                elif (unit == 'cm'):
-                    if not number_is_between(150, 193, hgt[:-2]):
-                        validation_infos = False
-                else:
-                    validation_infos = False
-
-
-                t += validation_infos
+                t += validate_infos(passport)
 
             # reset passport
             passport = {}
+            
+    t += validate_infos(passport)
 
     return t
 
